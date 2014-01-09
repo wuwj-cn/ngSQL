68ba84a03c9fa1c43d568879a0ac4acfabdeb472
wuwj-cn <wenjie.0617@gmail.com>
2014-01-09 21:01:39 +0800

    ngsql shutdown

---
+++ ngsql/src/main/server/org/ng12306/ngsql/NgSQLShutdown.java
@@ -0,0 +1,15 @@
+package org.ng12306.ngsql;
+
+import java.util.Date;
+
+/**
+ * 
+ * @author <a href="mailto:wenjie.0617@gmail.com">wuwj-cn</a>
+ */
+public final class NgSQLShutdown {
+
+    public static void main(String[] args) {
+        System.out.println(new Date() + ",server shutdown!");
+    }
+
+}
